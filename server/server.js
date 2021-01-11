47462d830ed220e960b93e749f6ee02ef0da2c80
Cameron Maclean <cm.maclean@yahoo.com>
2021-01-11 10:29:58 +0000

    Added to server file - completed server file setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
 // Server code goes here
+
+//Imports
+
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const createRouter = require('./helpers/create_router');
+
+app.use(parser.json());
+app.use(cors());
+
+MongoClient.connect("mongodb://localhost:27017")
+  .then((client) => {
+    const db = client.db('glasgow_street_art');
+    const muralCollection = db.collection('murals');
+    const muralRouter = createRouter(muralCollection);
+    app.use('/api/murals', muralRouter);
+  })
+  .catch(console.error);
+
+app.listen(3000, function() {
+  console.log(`Listening on port ${this.address().port}`);
+})
