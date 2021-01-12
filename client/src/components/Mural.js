3373620dc136c47ab18572d1c124b0515b6e02f1
Katie Strong <katiestrong123@gmail.com>
2021-01-12 19:58:51 +0000

    error: handleVisited is not defined

--- client/src/components/Mural.js
+++ client/src/components/Mural.js
@@ -1,8 +1,11 @@
 import {useState} from 'react';
 
 function Mural ({murals, mural, updateMural, instagramLink, instagram2Link}){
+    const [visitedButtonText, setVisitedButtonText] = useState("Not Visited");
+
     const [likeButtonText, setLikeButtonText] = useState("♥️");
     if (!mural) return null;
+    
 
     const handleLike = () => {
         let updatedValue = null;
@@ -14,7 +17,14 @@ function Mural ({murals, mural, updateMural, instagramLink, instagram2Link}){
             updatedValue = mural.likes -= 1;
             setLikeButtonText("♥️");
         }
-        
+    const handleVisited = () => {
+        if (visitedButtonText === "Not Visited") {
+            setVisitedButtonText("✔️Visited");
+        }
+        else {
+            setVisitedButtonText("Not Visited");
+        }
+                
         updateMural({
             _id: mural._id,
             name: mural.name,
@@ -25,11 +35,12 @@ function Mural ({murals, mural, updateMural, instagramLink, instagram2Link}){
             year: mural.year,
             lat: mural.lat,
             lng: mural.lng,
-            imageURL: mural.imageUrL,
+            imageURL: mural.imageUrL, 
             likes: updatedValue
         })
+    
+    }}
 
-    }
     const instagram2Node = instagram2Link ? (<p>Instagram: <a href={instagram2Link}>@{mural.instagram2}</a></p>): null
     return (
         <div className='mural-listing'>
@@ -42,7 +53,14 @@ function Mural ({murals, mural, updateMural, instagramLink, instagram2Link}){
             <p>{mural.location}</p>
             <p>{mural.description}</p>
             <p>Likes: {mural.likes}</p>
-            <button onClick={handleLike}>{likeButtonText}</button>
+            <button onClick={handleLike}>{likeButtonText}</button>    
+
+            
+                    
+            {/* <button onClick={handleVisited}>{visitedButtonText}</button> */}
+
+            {/* The error says that handleVisited is not defined */}
+
         </div>
     )
 }
