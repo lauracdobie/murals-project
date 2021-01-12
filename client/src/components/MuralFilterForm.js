9812008deddd4ec963bcdf266e727bd6dbb1ebf2
Cameron Maclean <cm.maclean@yahoo.com>
2021-01-12 12:18:13 +0000

    Added to mural filter form - added select drop down menu that captures user input

--- client/src/components/MuralFilterForm.js
+++ client/src/components/MuralFilterForm.js
@@ -3,11 +3,21 @@ const MuralFilterForm = ( { onUserInput } ) => {
     const userInput = event.target.value;
     onUserInput(userInput);
   };
+  const handleFilterSelect = (event) => {
+    const userInput = event.target.value;
+    onUserInput(userInput);
+  };
 
   return (
     <div id="mural-filter">
       <span>Mural Filter: </span>
-      <input  placeholder="Search for artist..." onChange={handleFilterInput} type="text" id="mural-input"/>
+      <select onChange={handleFilterSelect} name="mural-search-type" id="mural-filter-select">
+        <option value="">Select a filter...</option>
+        <option value="name">Name</option>
+        <option value="artist">Artist</option>
+        <option value="year">Year</option>
+      </select>
+      <input  placeholder="Enter keyword..." onChange={handleFilterInput} type="text" id="mural-input"/>
     </div>
   )
 
