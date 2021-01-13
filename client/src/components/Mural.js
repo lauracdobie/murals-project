d87894090ceeafceccea8ddcef17a736c84b4d7a
Cameron Maclean <cm.maclean@yahoo.com>
2021-01-13 19:49:31 +0000

    Fixed class reference to instagram links

--- client/src/components/Mural.js
+++ client/src/components/Mural.js
@@ -54,23 +54,29 @@ function Mural ({murals, mural, updateMural, instagramLink, instagram2Link, inst
 
     return (
         <div className={visitedClass} >
+
             <div className="mural-image-section">
                 <img className='mural-pic' src={mural.imageUrl} alt={mural.name}/>
             </div>
 
             <div className="mural-text">
+
                 <div className="mural-title">
                     <h3>{mural.name}</h3>
                 </div>
+
                 <div>
                     <p>{mural.artist}, {mural.year}</p>
-                    <div className="insta-section">
-                        <img className="insta-icon" src={InstagramIcon} />
-                        <p><a href={instagramLink}>@{mural.instagram}</a> {instagram2Node} {instagram3Node} </p>
-                    </div>
+                        <div className="insta-section">
+                            <img className="insta-icon" src={InstagramIcon} />
+                            <div className="insta-text">
+                                <p><a href={instagramLink}>@{mural.instagram}</a> {instagram2Node} {instagram3Node} </p>
+                            </div>
+                        </div>
                     <p>{mural.location}</p>
                     <p className="mural-description">{mural.description}</p>
                 </div>
+
             </div>
 
             <div className="mural-buttons">
