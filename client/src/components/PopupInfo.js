2fffa1022d206a4eed53107efe419c31479efc9e
Laura Dobie <moving.toyshop@gmail.com>
2021-01-14 08:32:56 +0000

    Styled popups

--- client/src/components/PopupInfo.js
+++ client/src/components/PopupInfo.js
@@ -1,17 +1,11 @@
-// import CatLady from "../assets/crazy_cat_lady.jpg";
-// import LostGiant from "../assets/lost_giant.jpg"
-// import DrConnolly from "../assets/dr_connolly.jpg"
-// import ShadowHandPuppet from "../assets/shadow_hand_puppets.jpg"
-import {useState} from 'react';
-
-import Mural from "./Mural";
+import './PopupInfo.css';
 
 function PopupInfo ({popupInfo, updatePopupInfo}){
     if (!popupInfo) return null;
 
     return (
         <div className='popup-listing'>
-            <img className='mural-pic' src={popupInfo.imageUrl} alt={popupInfo.name}/>
+            <img className='popup-pic' src={popupInfo.imageUrl} alt={popupInfo.name}/>
             <h3>{popupInfo.name}</h3>
             <p>{popupInfo.location}</p>
             <p>{popupInfo.artist}, {popupInfo.year}</p>
